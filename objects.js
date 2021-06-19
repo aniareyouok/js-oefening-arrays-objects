fb22272431c4a50b2fa2e2f3857970dac219527e
ani <aniareyouok@riseup.net>
2021-06-19 14:49:03 +0200

    added objects

--- objects.js
+++ objects.js
@@ -2,11 +2,24 @@
 // Maak een object met daarin jouw voornaam (string), achternaam (string), leeftijd (getal) en geboortedatum (date object). De datum hoeft niet geformat te worden.
 // Log dit in de terminal.
 
+const student1 = {
+  firstName: 'ani',
+  lastName: 'öhman',
+  currentAge: 41,
+  dateOfBirth: {
+    day: 24,
+    month: 'Mei',
+    year: 1980,
+  },
+}
+
+console.log(student1);
 
 // ===  Opdracht 2 ===
 // Zorg ervoor dat de volgende string in de terminal wordt gelogd:
 // "Hoi! Ik ben <voornaam>, ik ben <leeftijd> jaar oud."
 
+console.log('Hoi! Ik ben ' + student1.firstName + ', ik ben ' + student1.currentAge + ' jaar oud.')
 
 // === Opdracht 3: bekijk het volgende object ===
 
@@ -21,17 +34,25 @@ const product = {
 
 // 3a. Log de naam van de serie in de terminal.
 
+console.log(product.series);
+
 // 3b. Overschrijf de oude waarde van de property color ("creme") naar "roze". Log het object in de terminal om te checken of het gelukt is.
+
+product.color = 'roze';
+console.log(product.color);
 
 // 3c. Log de tweede materiaalsoort ("kunststof") in de terminal
 
+console.log(product.material[1]);
+
 // 1d. Log de afmetingen (b-h-d) in de terminal
 
+console.log(product['b-h-d']);
 
 // === Opdracht 4 ===
 // Declareer een variabele met daarin een string die het product beschrijft. Gebruik hiervoor de properties productName en material uit het object.
 // ---- Verwachte uitkomst:
 // "De Smeg Waterkoker is gemaakt van roestvrijstaal en kunststof"
 
-
+console.log('De Smeg Waterkoker is gemaakt van ' + product.material[0] + ' en ' + product.material[1] + '.')
 
